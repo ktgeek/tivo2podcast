5b505847f0542639404c67cb4cb54b065e49e028
Keith T. Garner <kgarner@kgarner.com>
2010-05-09 07:36:30 -0400

    Slight to make the RSS feed description not look assy

--- lib/tivo/tivopodcast.rb
+++ lib/tivo/tivopodcast.rb
@@ -421,7 +421,7 @@ module Tivo2Podcast
       # Here is where I would generate RSS and also clean up older files
       rss = RSS::Maker.make("2.0") do |maker|
         maker.channel.title = @config['show_name']
-        maker.channel.description = "My " + @config['show_name'] + "how RSS feed"
+        maker.channel.description = "My " + @config['show_name'] + " RSS feed"
         maker.channel.link = @config['rss_link']
         maker.channel.lastBuildDate = Time.now
 
