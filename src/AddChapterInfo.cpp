098a7f0ee73feb0dc5c5b10af2485f05ed45af23
Keith T. Garner <kgarner@kgarner.com>
2011-01-23 12:17:08 -0600

    Add copyright and license statement to AddChapterInfo

--- src/AddChapterInfo.cpp
+++ src/AddChapterInfo.cpp
@@ -1,10 +1,23 @@
+/*
+ * Copyright 2011 Keith T. Garner. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ */
 #include <fstream>
 #include <string>
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
 #include <mp4v2/mp4v2.h>
-
-// g++  AddChapterInfo.cpp -o AddChapterInfo -I/opt/local/include -L/opt/local/lib -lmp4v2 -lboost_regex
 
 int main(int argc, char *argv[])
 {
