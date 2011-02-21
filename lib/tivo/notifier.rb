7ce0523b4338afcd9978956596fffaa46b8b0059
Keith T. Garner <kgarner@kgarner.com>
2011-02-21 14:43:10 -0600

    License and comment cleanup

--- lib/tivo/notifier.rb
+++ lib/tivo/notifier.rb
@@ -1,4 +1,16 @@
-# Base notifier class, has all the common methods
+# Copyright 2011 Keith T. Garner. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
 
 module TiVo2Podcast
   class Notifier
