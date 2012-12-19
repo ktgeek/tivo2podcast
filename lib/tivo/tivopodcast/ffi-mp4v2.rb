5bd61ccb5de63feadfac4facfa9b46c52d531410
Keith T. Garner <kgarner@kgarner.com>
2012-12-19 17:51:39 -0600

    Start to write some documentation for the mp4 methods. Most text taking from libmp4v2

--- lib/tivo/tivopodcast/ffi-mp4v2.rb
+++ lib/tivo/tivopodcast/ffi-mp4v2.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'ffi'
 
 # This module uses FFI to wrap the mp4v2 functions needed by the
@@ -13,10 +14,33 @@ module Mp4v2
   typedef :uint32, :MP4TrackId
   typedef :uint64, :MP4Duration
 
+  MP4_INVALID_FILE_HANDLE = nil
+  
   attach_function(:c_mp4_modify, :MP4Modify, [:string, :uint32, :uint32],
                   :MP4FileHandle)
-  def Mp4v2.mp4_modify(filename, verbosity = 0, flags = 0)
-    c_mp4_modify(filename, verbosity, flags)
+  # Modify an existing mp4 file.
+  #
+  # MP4Modify is the first call that should be used when you want to modify
+  # an existing mp4 file. It is roughly equivalent to opening a file in
+  # read/write mode.
+  #
+  # Since modifications to an existing mp4 file can result in a sub−optimal
+  # file layout, you may want to use MP4Optimize() after you have  modified
+  # and closed the mp4 file.
+  #
+  # fileName:: pathname of the file to be modified.
+  #
+  # verbosity:: bitmask of diagnostic details the library should print to stdout
+  # during its functioning.
+  #
+  # @return On success a handle of the target file for use in subsequent calls
+  # to the library. On error, #MP4_INVALID_FILE_HANDLE.
+  #
+  # @see MP4SetVerbosity() for <b>verbosity</b> values.
+  def Mp4v2.mp4_modify(filename, verbosity = 0)
+    # Per mp4v2 documentation, flags is currently ignored, so we won't
+    # even expose that to ruby and just pass it as 0 all the time.
+    c_mp4_modify(filename, verbosity, 0)
   end
 
   attach_function(:c_mp4_optimize, :MP4Optimize, [:string, :string, :uint32],
