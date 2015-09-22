f74de440c53d85034463c84b8c18cb9f5e3158b6
Keith T. Garner <kgarner@kgarner.com>
2015-09-22 09:58:22 -0500

    Change testing for character stripping into its own test

--- spec/lib/tivo/tivopodcast/main_engine_spec.rb
+++ spec/lib/tivo/tivopodcast/main_engine_spec.rb
@@ -6,7 +6,7 @@ describe Tivo2Podcast::MainEngine do
     let(:main_engine) { Tivo2Podcast::MainEngine.new(config) }
     let(:show) do
       show = instance_double("TiVo::TiVoVideo")
-      allow(show).to receive(:title) { "Who: Revisited" }
+      allow(show).to receive(:title) { "Show" }
       allow(show).to receive(:time_captured) { Time.new(2015, 7, 10, 22) }
       allow(show).to receive(:episode_title) { nil }
       allow(show).to receive(:episode_number) { nil }
@@ -16,29 +16,36 @@ describe Tivo2Podcast::MainEngine do
     it "returns a title for a show without episode_title or episode_number" do
       name = main_engine.create_show_base_filename(show)
 
-      expect(name).to eql "Who_ Revisited-201507102200"
+      expect(name).to eql "Show-201507102200"
+    end
+
+    it "strips out problematic characters" do
+      allow(show).to receive(:title) { "A:b?c;" }
+      name = main_engine.create_show_base_filename(show)
+
+      expect(name).to eql "A_b_c_-201507102200"
     end
 
     it "returns a title for a show with an episode_title" do
-      allow(show).to receive(:episode_title) { "Title?" }
+      allow(show).to receive(:episode_title) { "Title" }
       name = main_engine.create_show_base_filename(show)
 
-      expect(name).to eql "Who_ Revisited-201507102200-Title_"
+      expect(name).to eql "Show-201507102200-Title"
     end
 
     it "returns a title for a show with an episode_number" do
       allow(show).to receive(:episode_number) { "31337" }
       name = main_engine.create_show_base_filename(show)
 
-      expect(name).to eql "Who_ Revisited-201507102200-31337"
+      expect(name).to eql "Show-201507102200-31337"
     end
 
     it "returns a title for a show with both an episode title and number" do
-      allow(show).to receive(:episode_title) { "Title?" }
+      allow(show).to receive(:episode_title) { "Title" }
       allow(show).to receive(:episode_number) { "31337" }
 
       name = main_engine.create_show_base_filename(show)
-      expect(name).to eql "Who_ Revisited-201507102200-Title_-31337"
+      expect(name).to eql "Show-201507102200-Title-31337"
     end
   end
 end
