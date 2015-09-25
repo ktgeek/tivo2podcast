9e88b6553a05eaa77b4b4de6e4eeb75fb618bb0b
Keith T. Garner <kgarner@kgarner.com>
2015-09-25 10:28:38 -0400

    Move all Work Order stuff into its own file

--- lib/tivo/tivopodcast/main_engine.rb
+++ lib/tivo/tivopodcast/main_engine.rb
@@ -20,6 +20,7 @@ require 'tivopodcast/notifier'
 require 'tivopodcast/transcoder'
 require 'tivopodcast/database'
 require 'tivopodcast/rss_generator'
+require 'tivopodcast/main_engine_work_orders.rb'
 
 module Tivo2Podcast
   class MainEngine
@@ -27,93 +28,14 @@ module Tivo2Podcast
       @t2pconfig = t2pconfig ? t2pconfig : Tivo2Podcast::Config.instance
     end
 
-    class WorkOrder
-      attr_reader :type, :config
-      def initialize(config)
-        @config = config
-        @type = nil
-      end
-    end
-
-    class NoMoreWorkOrder < WorkOrder
-      def initialize
-        super(nil)
-        @type = :NO_MORE_WORK
-      end
-    end
-
-    class TranscodeWorkOrder < WorkOrder
-      attr_reader :show, :basename, :download, :transcode
-
-      def initialize(config, show, basename, download, transcode)
-        super(config)
-        @show, @basename = show, basename
-        @download, @transcode = download, transcode
-        @type = :TRANSCODE
-      end
-
-      def do_work
-        # I need config, s/show, basename, download, transcode
-        notifier = Tivo2Podcast::NotifierEngine.instance
-        notifier.notify("Starting transcode of #{@basename}")
-
-        transcoder = Tivo2Podcast::Transcoder.new(@config, @show)
-        transcoder.transcode_show(@download, @transcode)
-
-        transcoder.skip_commercials(@basename, @download, @transcode)
-
-        File.delete(@download) if File.exists?(@download)
-
-        show =
-          Tivo2Podcast::Db::Show.new_from_config_show_filename(@config, @show,
-                                                               @transcode)
-        show.save!
-        notifier.notify("Finished transcode of #{@basename}")
-      end
-    end
-
-    class CleanupWorkOrder < WorkOrder
-      def initialize(config)
-        super(config)
-        @type = :CLEANUP
-      end
-
-      def do_work
-        newest_shows = Tivo2Podcast::Db::Show.where(configid: @config,
-          on_disk: true).order(s_ep_timecap: :desc).limit(
-          @config.ep_to_keep)
-        unless newest_shows.nil? || newest_shows.empty?
-          Tivo2Podcast::Db::Show.where(configid: @config,
-            on_disk: true).where.not(id: newest_shows).each do |show|
-            # If the file doesn't exist, don't try to delete, but
-            # still setting the on_disk to false is appropriate.
-            File.delete(show.filename) if File.exists?(show.filename)
-            show.on_disk = false
-            show.save!
-          end
-        end
-
-        # We might want these to move...or to rename this object to
-        # something more sane.
-        Tivo2Podcast::RssGenerator.generate_from_config(@config)
-        # Put notification here
-        Tivo2Podcast::NotifierEngine.instance.notify(
-          "Finished processing #{@config.config_name}")
-      end
-    end
-
-    # This method feels like its still doing too much...
     def create_work_thread(queue)
       raise ArgumentError if queue.nil?
       Thread.new do
         loop do
           work_order = queue.deq
+          break if work_order.type == :NO_MORE_WORK
 
-          if work_order.type == :NO_MORE_WORK
-            break
-          else
-            work_order.do_work
-          end
+          work_order.do_work
         end
       end
     end
