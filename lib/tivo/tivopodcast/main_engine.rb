55b1de5b9db6e98f186700ef118e68bb0b93702a
Keith T. Garner <kgarner@kgarner.com>
2015-09-26 10:13:59 -0400

    More minor attempts to simply normal_processing

--- lib/tivo/tivopodcast/main_engine.rb
+++ lib/tivo/tivopodcast/main_engine.rb
@@ -34,7 +34,6 @@ module Tivo2Podcast
         loop do
           work_order = queue.deq
           break if work_order.type == :NO_MORE_WORK
-
           work_order.do_work
         end
       end
@@ -73,7 +72,6 @@ module Tivo2Podcast
 
     def get_shows_to_process(tivo, config)
       shows = tivo.get_shows_by_name(config.show_name)
-
       # Only work on the X latest shows.  That way if there are 10
       # on the tivo, but we only want to keep 4, we don't encode 6
       # of them just to throw them out later in the cleanup phase.
@@ -86,36 +84,28 @@ module Tivo2Podcast
 
     # This method is doing WAY WAY WAY too much
     def normal_processing
-      configs = get_configs
-
       work_queue = Queue.new
       work_thread = create_work_thread(work_queue)
 
       tivo = @t2pconfig.tivo_factory
 
-      configs.each do |config|
+      get_configs.each do |config|
         shows = get_shows_to_process(tivo, config)
 
         # So starts the giant loop that processes the shows...
         shows.each do |s|
           basename = create_show_base_filename(s)
-
           download = "#{basename}.mpg"
           transcode = "#{basename}.m4v"
 
           notifier = Tivo2Podcast::NotifierEngine.instance
-          # We'll need the later condition until everything has a program_id
-          # (this is only for my own migration.)
           unless (Tivo2Podcast::Db::Show.where(
-                    configid: config, s_ep_programid: s.program_id).exists? ||
-                  File.exist?(transcode))
+                  configid: config, s_ep_programid: s.program_id).exists?)
             begin
               notifier.notify("Starting download of #{basename}")
-
               # If the file exists, we'll assume the download went okay
               # Shame on us for not checking if it isn't
               download_show(s, download) unless File.exists?(download)
-
               notifier.notify("Finished download of #{basename}")
 
               work_queue.enq(TranscodeWorkOrder.new(config, s, basename,
@@ -139,7 +129,6 @@ module Tivo2Podcast
       end
 
       work_queue.enq(NoMoreWorkOrder.new)
-
       work_thread.join
       Tivo2Podcast::NotifierEngine.instance.shutdown
     end
