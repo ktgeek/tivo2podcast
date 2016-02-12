b6ff85bcee89142513e2ae9bb5c7321c6e54a0d0
Keith T. Garner <kgarner@kgarner.com>
2016-02-12 06:34:21 -0600

    Move to using symbols for the keys and kill the hash rockets

--- lib/tivo/tivopodcast/config.rb
+++ lib/tivo/tivopodcast/config.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2011 Keith T. Garner. All rights reserved.
+# Copyright 2016 Keith T. Garner. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -34,21 +34,22 @@ module Tivo2Podcast
     # base config from.
     def initialize
       @config = {
-        "tivo_addr" => nil,
-        "tivo_name" => nil,
-        "mak" => nil,
-        "verbose" => false,
-        "opt_config_names" => Array.new,
-        "tivodecode" => 'tivodecode',
-        "handbrake" => 'HandBrakeCLI',
-        "cleanup" => false,
-        "atomicparsley" => 'AtomicParsley',
-        "comskip" => nil,
-        "comskip_ini" => nil,
-        "baseurl" => nil,
-        "aggregate_file" => nil,
-        "notifiers" => Array.new,
-        "regenerate_rss" => false
+        tivo_addr: nil,
+        tivo_name: nil,
+        mak: nil,
+        verbose: false,
+        opt_config_names: Array.new,
+        tivodecode: 'tivodecode',
+        handbrake: 'HandBrakeCLI',
+        cleanup: false,
+        atomicparsley: 'AtomicParsley',
+        comskip: nil,
+        comskip_ini: nil,
+        baseurl: nil,
+        aggregate_file: nil,
+        notifiers: Array.new,
+        regenerate_rss: false,
+        console: false
       }
 
       load_from_file(CONFIG_FILENAME)
@@ -67,66 +68,69 @@ module Tivo2Podcast
     end
 
     def tivo_addr=(value)
-      @config['tivo_addr'] = value
+      @config[:tivo_addr] = value
     end
 
     # Returns the tivo_address defined in the config.  If one is not
     # defined in the config, try to locate the tivo vi dnssd
     def tivo_addr
-      if @config['tivo_addr'].nil?
+      if @config[:tivo_addr].nil?
         puts "Attemping to locate tivo #{@config['tivo_name'] unless @config['tivo_name'].nil?}..." if @config['verbose']
-        tmp = TiVo.locate_via_dnssd(@config['tivo_name'])
+        tmp = TiVo.locate_via_dnssd(@config[:tivo_name])
         if tmp.nil?
-          puts "TiVo not found!" if @config['verbose']
+          puts "TiVo not found!" if @config[:verbose]
           # Should be changed to an exception to be throw
           printf($stderr, "TiVo hostname or IP required to run the script\n")
           exit(1)
         else
           puts "TiVo found at #{tmp}" if @config['verbose']
-          @config['tivo_addr'] = tmp
+          @config[:tivo_addr] = tmp
         end
       end
 
-      result = @config['tivo_addr']
+      result = @config[:tivo_addr]
       # If the tivo_addr is NOT a dotted quad, do a DNS lookup for the
       # IP. The TiVo wants us to pass the IP address for whatever reason.
       # I should use bounjour/ZeroConf to find the local tivo
       if /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(result).nil?
         result = IPSocket.getaddress(result)
       end
-      
-      return result
+      result
     end
 
     def tivo_name=(value)
-      @config['tivo_name'] = value
+      @config[:tivo_name] = value
     end
 
     def mak=(value)
-      @config['mak'] = value
+      @config[:mak] = value
     end
 
     # Returns the mak from the configuration file or look it up via
     # the .tivodecode_make file
     def mak
-      if @config['mak'].nil?
+      if @config[:mak].nil?
         # Load the mak if we have a mak file
         mak_file = ENV['HOME'] + '/.tivodecode_mak'
-        @config['mak'] = File.read(mak_file).strip if File.exist?(mak_file)
+        @config[:mak] = File.read(mak_file).strip if File.exist?(mak_file)
       end
-      return @config['mak']
+      @config[:mak]
     end
 
     def verbose=(value)
-      @config['verbose'] = value
+      @config[:verbose] = value
     end
 
     def cleanup=(value)
-      @config['cleanup'] = value
+      @config[:cleanup] = value
     end
 
     def regenerate_rss=(value)
-      @config['regenerate_rss'] = value
+      @config[:regenerate_rss] = value
+    end
+
+    def console=(value)
+      @config[:console] = value
     end
 
     # For backward compatibility with Config from when more things
@@ -135,8 +139,8 @@ module Tivo2Podcast
     # called.  If there's nothing in the hash, we'll call the normal
     # method_missing to throw the exception.
     def method_missing(method, *params)
-      method_name = method.to_s
-      return @config[method_name] if @config.keys.include?(method_name)
+      value = @config[method.to_sym]
+      return value unless value.nil?
       super
     end
 
