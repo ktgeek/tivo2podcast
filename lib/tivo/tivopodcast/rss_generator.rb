437657c0a7746a003403ab1f1f8c3d27245b9cce
Keith T. Garner <kgarner@kgarner.com>
2016-04-03 14:42:56 -0400

    refactor rss_generator and add start of spec file for it.

--- lib/tivo/tivopodcast/rss_generator.rb
+++ lib/tivo/tivopodcast/rss_generator.rb
@@ -23,68 +23,78 @@ require 'tivopodcast/database'
 module Tivo2Podcast
   # Generates the video podcast feed.
   class RssGenerator
-    def RssGenerator.regenerate_rss_files
+    def self.regenerate_rss_files
       rss_files = Tivo2Podcast::RssFile.all
-      RssGenerator.generate_from_rssfiles(rss_files)
+      self.generate_from_rssfiles(rss_files)
     end
 
-    def RssGenerator.generate_from_config(config)
-      RssGenerator.generate_from_rssfiles(config.rss_files)
+    def self.generate_from_config(config)
+      self.generate_from_rssfiles(config.rss_files)
     end
 
-    def RssGenerator.generate_from_configs(configs)
-      RssGenerator.generate_from_rssfiles(configs.accumulate.rss_files)
+    def self.generate_from_configs(configs)
+      self.generate_from_rssfiles(configs.accumulate.rss_files)
     end
 
-    def RssGenerator.generate_from_rssfiles(rssfiles)
+    def self.generate_from_rssfiles(rssfiles)
       rssfiles.each do |rss_file|
-        rss = RSS::Maker.make("2.0") do |maker|
-          maker.channel.title = rss_file.feed_title
-          maker.channel.description = rss_file.feed_description
-          maker.channel.link = rss_file.link
-          maker.channel.lastBuildDate = Time.now
+        rss = self_and_parents.new(rss_file).make_rss
 
-          maker.channel.itunes_author = maker.channel.title
-          maker.channel.itunes_owner.itunes_name = rss_file.owner_name
-          maker.channel.itunes_owner.itunes_email = rss_file.owner_email
-
-          maker.items.do_sort = true
-
-          shows = Tivo2Podcast::Show.where(configid: rss_file.configs, on_disk: true)
-            .order(:s_ep_timecap)
-
-          shows.each do |show|
-            # If the file got removed and no one ran a database clean
-            # let's not add it to the RSS feed.
-            if File.exists?(show.filename)
-              maker.items.new_item do |item|
-                if rss_file.configs.size > 1
-                  item.title = show.s_name + ": " + show.s_ep_title
-                else
-                  item.title = show.s_ep_title
-                end
-                item.link = URI.escape(rss_file.base_url + show.filename)
-
-                item.guid.content = item.link
-                item.guid.isPermaLink = true
-                item.pubDate = Time.at(show.s_ep_timecap)
-                item.description = show.s_ep_description
-                item.itunes_summary = show.s_ep_description
-                item.itunes_explicit = "No"
-
-                item.itunes_duration =
-                  TiVo::TiVoVideo.human_duration(show.s_ep_length)
-
-                item.enclosure.url = item.link
-                item.enclosure.length = File.size(show.filename)
-                item.enclosure.type = 'video/x-m4v'
-              end
-            end
-          end
-        end
-
-        # this code needs to change to writing the file out.
         File.open(rss_file.filename, 'w') { |f| f << rss.to_s }
+      end
+    end
+
+    def initialize(rss_file)
+      @rss_file = rss_file
+    end
+
+    def make_rss
+      RSS::Maker.make("2.0") do |maker|
+        configure_channel(maker.channel, @rss_file)
+        maker.items.do_sort = true
+        shows = Tivo2Podcast::Db::Show.where(configid: @rss_file.configs,
+                                             on_disk: true).order(:s_ep_timecap)
+        shows.each { |s| add_item(maker.items, s) }
+      end
+    end
+
+    private
+    def configure_channel(channel)
+      channel.title = @rss_file.feed_title
+      channel.description = @rss_file.feed_description
+      channel.link = @rss_file.link
+      channel.lastBuildDate = Time.now
+
+      channel.itunes_author = channel.title
+      channel.itunes_owner.itunes_name = @rss_file.owner_name
+      channel.itunes_owner.itunes_email = @rss_file.owner_email
+    end
+
+    def item_title(size, show)
+      size > 1 ? "#{show.s_name}: #{show.s_ep_title}" : show.s_ep_title
+    end
+
+    def add_item(items, show)
+      # If the file got removed and no one ran a database clean
+      # let's not add it to the RSS feed.
+      return unless File.exist?(show.filename)
+      items.new_item do |item|
+        item.title = item_title(@rss_file.configs.size, show)
+        item.link = URI.escape(@rss_file.base_url + show.filename)
+
+        item.guid.content = item.link
+        item.guid.isPermaLink = true
+        item.pubDate = Time.at(show.s_ep_timecap)
+        item.description = show.s_ep_description
+        item.itunes_summary = show.s_ep_description
+        item.itunes_explicit = "No"
+
+        item.itunes_duration =
+          TiVo::TiVoVideo.human_duration(show.s_ep_length)
+
+        item.enclosure.url = item.link
+        item.enclosure.length = File.size(show.filename)
+        item.enclosure.type = 'video/x-m4v'
       end
     end
   end
---
+++ spec/lib/tivo/tivopodcast/rss_generator_spec.rb
@@ -0,0 +1,57 @@
+require 'tivo/tivopodcast/rss_generator'
+
+describe Tivo2Podcast::RssGenerator do
+  describe "#item_title" do
+    let(:show) do
+      show = double("TiVo2Podcast::Db::Show")
+      allow(show).to receive(:s_name) { "Show" }
+      allow(show).to receive(:s_ep_title) { "Title" }
+      show
+    end
+    let(:builder) { Tivo2Podcast::RssGenerator.new(nil) }
+
+    it "returns the episode title if the size is 1" do
+      title = builder.send(:item_title, 1, show)
+      expect(title).to eql "Title"
+    end
+
+    it "returns the show prepended to the title when size is > 1" do
+      title = builder.send(:item_title, 2, show)
+      expect(title).to eql "Show: Title"
+    end
+  end
+
+  context "make the rss content" do
+    let(:maker) { RSS::Maker["2.0"].new }
+    let(:rss_file) do
+      rss_file = double("Tivo2Podcast::RssFile")
+      allow(rss_file).to receive(:feed_title) { "Title" }
+      allow(rss_file).to receive(:feed_description) { "description" }
+      allow(rss_file).to receive(:link) { "http://example.com" }
+      allow(rss_file).to receive(:owner_name) { "owner" }
+      allow(rss_file).to receive(:owner_email) { "example@example.com" }
+      rss_file
+    end
+    let(:builder) { Tivo2Podcast::RssGenerator.new(rss_file) }
+
+    describe "#configure_channel" do
+      let(:channel) { maker.channel }
+
+      it "fills out rss channel with expected information" do
+        builder.send(:configure_channel, channel)
+
+        expect(channel.title).to eql("Title")
+        expect(channel.description).to eql("description")
+        expect(channel.link).to eql("http://example.com")
+        expect(channel.itunes_author).to eql("Title")
+        expect(channel.itunes_owner.itunes_name).to eql("owner")
+        expect(channel.itunes_owner.itunes_email).to eql("example@example.com")
+      end
+    end
+
+    # TODO: finishing testing othre methodsZ
+#    describe "#make_item" do
+#      let(:item)
+#    end
+  end
+end
